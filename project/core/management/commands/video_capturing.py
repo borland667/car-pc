96e27da18e5092cbf9b0198c01947acfc2bd316c
Telminov Sergey <telminov@soft-way.biz>
2014-05-17 00:40:34 +0400

    #3 video capturing via picamera

--- project/core/management/commands/video_capturing.py
+++ project/core/management/commands/video_capturing.py
@@ -7,6 +7,7 @@ import signal
 
 from django.core.management.base import NoArgsCommand
 from django.conf import settings
+import picamera
 
 from core import models
 
@@ -18,32 +19,37 @@ class Command(NoArgsCommand):
         while True:
             # check need to tart capturing
             if self._is_started():
-                return_code = self._start_capturing()
+                # return_code = self._start_capturing()
+                self._start_capturing()
 
-                if self._is_started() and return_code != 0:
-                    # try kill other for restarting
-                    subprocess.check_call('killall streamer', shell=True)
-                    time.sleep(1)
+                # if self._is_started() and return_code != 0:
+                #     # try kill other for restarting
+                #     subprocess.check_call('killall streamer', shell=True)
+                #     time.sleep(1)
 
             # wait and retry check
             else:
-                time.sleep(0.1)
+                time.sleep(1)
 
     def _start_capturing(self):
+        camera = picamera.PiCamera()
 
         video_path = self._get_path()
-        command = 'streamer -q -c /dev/video0 -f jpeg -t 00:05:00 -s 640x480 -o %s' % video_path
-        p = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, preexec_fn=os.setsid)
+        # command = 'streamer -q -c /dev/video0 -f jpeg -t 00:05:00 -s 640x480 -o %s' % video_path
+        # p = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True, preexec_fn=os.setsid)
+        camera.start_recording(video_path, format='mjpeg')
         print 'Started'
-        while p.poll() is None:
+        # while p.poll() is None:
+        while True:
             if self._is_started():
-                time.sleep(0.1)
+                time.sleep(1)
             else:
-                os.killpg(p.pid, signal.SIGTERM)
+                camera.stop_recording()
+                # os.killpg(p.pid, signal.SIGTERM)
                 print 'Stopped'
                 return
-        print 'End: %s' % p.returncode
-        return p.returncode
+        # print 'End: %s' % p.returncode
+        # return p.returncode
 
 
     def _is_started(self):
@@ -58,5 +64,5 @@ class Command(NoArgsCommand):
         if not os.path.exists(settings.VIDEO_PATH):
             os.makedirs(settings.VIDEO_PATH)
         # full path to file
-        output_path = os.path.join(settings.VIDEO_PATH, self._curr_dt()) + '.avi'
+        output_path = os.path.join(settings.VIDEO_PATH, self._curr_dt()) + '.mjpeg'
         return output_path
